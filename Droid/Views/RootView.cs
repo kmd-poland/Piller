a48f27c5bf1af9e5482ac26f9f35ad632e0725cd
Michael Mazur <mmu@kmd.dk>
2017-07-05 14:11:14 +0200

    Added default tab on app start depending on whether there are any meds added or not Changed RootView in the way that now it retains it's state upon screen orientation change

--- Droid/Views/RootView.cs
+++ Droid/Views/RootView.cs
@@ -14,12 +14,16 @@ using Android.Widget;
 using MvvmCross.Droid.Support.V7.AppCompat;
 using Toolbar = Android.Support.V7.Widget.Toolbar;
 using Piller.ViewModels;
+using Android.Content.PM;
+using Android.Support.Design.Internal;
 
 namespace Piller.Droid.Views
 {
-    [Activity(Label = "RootView")]
+    [Activity(Label = "RootView", ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.KeyboardHidden)]
     public class RootView : MvxCachingFragmentCompatActivity<RootViewModel>
     {
+        private BottomNavigationView bottomNavigation;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -27,8 +31,8 @@ namespace Piller.Droid.Views
             SetContentView(Resource.Layout.MainLayout);
             var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
-            var bottomNavigation = FindViewById<BottomNavigationView>(Resource.Id.bottom_navigation);
-            bottomNavigation.NavigationItemSelected += (object sender, BottomNavigationView.NavigationItemSelectedEventArgs e) => {
+            this.bottomNavigation = FindViewById<BottomNavigationView>(Resource.Id.bottom_navigation);
+            this.bottomNavigation.NavigationItemSelected += (object sender, BottomNavigationView.NavigationItemSelectedEventArgs e) => {
 
                 switch (e.Item.ItemId)
 				{
@@ -46,7 +50,43 @@ namespace Piller.Droid.Views
 						break;
 				}
             };
+
+			this.ViewModel.ChangeTab.RegisterHandler(context =>
+			{
+                var menuId = -1; 
+                switch (context.Input)
+				{
+					case 0:
+                        menuId = Resource.Id.upcoming;
+						break;
+					case 1:
+						menuId = Resource.Id.medical_card;
+						break;
+					case 2:
+						menuId = Resource.Id.holidays;
+						break;
+					default:
+						break;
+				}
+
+                if (menuId != -1)
+                {
+                    this.RunOnUiThread(() =>
+                    {
+                        this.bottomNavigation.FindViewById(menuId)?.PerformClick();
+                        this.bottomNavigation.Invalidate();
+                    });
+                }
+			});
+
+            this.ViewModel.ShowInitialView.Execute().Subscribe();
         }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+        }
+
         public override bool OnCreateOptionsMenu(IMenu menu)
         {
             this.MenuInflater.Inflate(Resource.Menu.main_menu, menu);
--- ViewModels/RootViewModel.cs
+++ ViewModels/RootViewModel.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿﻿using System;
+using System.Linq;
 using System.Reactive;
 using System.Reactive.Linq;
+using System.Threading.Tasks;
 using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using Piller.Data;
+using Piller.Services;
 using ReactiveUI;
 
 namespace Piller.ViewModels
@@ -12,12 +17,31 @@ namespace Piller.ViewModels
         public ReactiveCommand<Unit, bool> ShowMedicalCardView { get; }
         public ReactiveCommand<Unit, bool> ShowHolidayView { get; }
         public ReactiveCommand<Unit, bool> GoSettings { get; set; }
+        public ReactiveCommand<Unit, Unit> ShowInitialView { get; }
+        public Interaction<int, Unit> ChangeTab { get; }
 
         public RootViewModel() {
             ShowUpcomingMedicationView = ReactiveCommand.Create(() => this.ShowViewModel<RegistrationViewModel>());
             ShowMedicalCardView = ReactiveCommand.Create(() => this.ShowViewModel<MedicalCardViewModel>());
             ShowHolidayView = ReactiveCommand.Create(() => this.ShowViewModel<HolidayViewModel>());
             GoSettings = ReactiveCommand.Create(() => this.ShowViewModel<SettingsViewModel>());
+
+            this.ChangeTab = new Interaction<int, Unit>();
+
+            this.ShowInitialView = ReactiveCommand.Create(() =>
+            {
+                Task.Run(async () =>
+                {
+                    if ((await Mvx.Resolve<IPermanentStorageService>().List<MedicationDosage>()).Any())
+                    {
+                        await this.ChangeTab.Handle(0);
+                    }
+                    else
+                    {
+                        await this.ChangeTab.Handle(1);
+                    }
+                });
+            });
         }
     }
 }
